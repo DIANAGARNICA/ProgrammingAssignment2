0d0752b4aa8480b0e0cea32467f476ff34cf7245
diana-garnica <dmgarnicac@correo.udistrital.edu.co>
2020-09-10 21:54:24 -0500

    COPIA DE LA FUNCION CREADA

---
+++ almacenarcache.R
@@ -0,0 +1,33 @@
+matrizcache<- function (x=matrix()){
+  inv<-NULL
+  SET<-function(y){
+    x<<-y
+    inv<<- NULL
+  }
+  obtener<- function () {x}
+  setInverse<- function(inverse){inv<<-inverse}
+  getInverse<-function(){inv}
+  list (set=SET, get=obtener, setInverse=setInverse, getInverse=getInverse)
+  
+}
+
+Resolvercache <-  function(x, ...) {
+  inv <- x$getInverse()
+  if (!is.null(inv)){
+    message("obteniendo datos del caché")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+  }
+
+
+
+
+
+
+
+
+
